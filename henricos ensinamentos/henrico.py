42ad40dac989b2f5e739e3ffbfe47db27f194e8f
ItstheNiko <thales.w.cp12@gmail.com>
2024-10-23 18:39:58 -0300

    xssq

---
+++ henricos ensinamentos/henrico.py
@@ -0,0 +1,2 @@
+a=['itstheniko', 'xesque', 'thalo e feio', 'eu', 10]
+print(a[:3])
---
+++ henricos ensinamentos/henrico2.py
@@ -0,0 +1,3 @@
+informacoes_prova=[['thalo', 7],['niko', 9]]#Informacoes dos resultados das provas
+if informacoes_prova[1][1] > 8:
+    print('parabens vc passou!')

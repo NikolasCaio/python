8430120c6782791e1cc4c9c90fe72f5a1a55c70f
ItstheNiko <thales.w.cp12@gmail.com>
2024-10-28 18:45:10 -0300

    Create henrico_funcoes.py

---
+++ henricos ensinamentos/henrico_funcoes.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+def texto_azul(texto_que_vai_ficar_azul):
+    print(Fore.BLUE, texto_que_vai_ficar_azul, Fore.RESET)
+texto_azul('thalo e gordo')
+def texto_red(texto_que_vai_ficar_vermeio):
+    print(Fore.RED, texto_que_vai_ficar_vermeio, Fore.RESET)
+texto_red('henrico e fofo')

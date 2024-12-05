d31191620f2de361a75def9deb9245d03dcf12a8
ItstheNiko <thales.w.cp12@gmail.com>
2024-12-05 17:39:39 -0300

    xesq

--- projetos/consultapags/projeto relatorio empresa/xesque.py
+++ projetos/consultapags/projeto relatorio empresa/xesque.py
@@ -9,7 +9,7 @@ chrome_options.add_experimental_option('prefs',{
     # nao pedir permissao para download
     'download.prompt_for_download': False,
     #setar local padrao para armazenar downloads
-    'download.default_directory': r'C:\\Users\\thale\\Documents\\Estudos\\python\\projetos\\consultapags\\projeto relatorio empresa\\relatorios',
+    'download.default_directory': r'C:\Users\thale\Documents\Estudos\python\projetos\consultapags\projeto relatorio empresa\relatorios',
     # nao pedir permissao para realizar multipos downloads
     'profile.default_content_settings_values.automatic_downloads': 1
 })
@@ -36,31 +36,35 @@ sleep(5)
 
 
 # 3 - Baixar o pdf de cada empresa e apos isso, renomear cada um deles para o nome da empresa do qual aquele relatorio pertence
-# 3.1 - Extrair o nome de cada empresa e guardar isso(numa lista)
-nomes_empresas = driver.find_elements(By.XPATH, "//td[@name='nome_empresa']")
-sleep(2)
-botao_download_pdf = driver.find_elements(By.XPATH, "//button[@class='download-btn']")
-sleep(2)
-
-for nome, botao_pdf in zip(nomes_empresas, botao_download_pdf):
-    # Renomear o arquivo  de "perfil_empresa.pdf" para "nome_empresa.pdf"
-    # 3.2 - A cada momento que eu baixar um arquivo, vou renomear ele para o nome daquela empresa
-    botao_pdf.click()
+def baixar_relatorios_das_empresas(driver):
+     # 3.1 - Extrair o nome de cada empresa e guardar isso(numa lista)
+    nomes_empresas = driver.find_elements(By.XPATH, "//td[@name='nome_empresa']")
     sleep(2)
-    # Aguardo o download finalizar
-    # Pego o nome do arquivo antigo e mudo para nome_da_empresa.pdf
+    botao_download_pdf = driver.find_elements(By.XPATH, "//button[@class='download-btn']")
+    sleep(2)
 
-    diretorio = r'C:\\Users\\thale\Documents\\Estudos\\python\\projetos\\consultapags\\projeto relatorio empresa\\xesque.py'       
-    nome_antigo = 'perfil_empresa.pdf'
-    novo_nome = f'{nome.text}.pdf'
+    for nome, botao_pdf in zip(nomes_empresas, botao_download_pdf):
+        # Renomear o arquivo  de "perfil_empresa.pdf" para "nome_empresa.pdf"
+        # 3.2 - A cada momento que eu baixar um arquivo, vou renomear ele para o nome daquela empresa
+        botao_pdf.click()
+        sleep(2)
+        # Aguardo o download finalizar
+        # Pego o nome do arquivo antigo e mudo para nome_da_empresa.pdf
 
-    caminho_completo_antigo = os.path.join(diretorio, nome_antigo)
-    caminho_completo_novo = os.path.join(diretorio, novo_nome)
-  
-    os.rename(caminho_completo_antigo, caminho_completo_novo)
+        diretorio = r'C:\Users\thale\Documents\Estudos\python\projetos\consultapags\projeto relatorio empresa\relatorios'       
+        nome_antigo = 'perfil_empresa.pdf'
+        novo_nome = f'{nome.text}.pdf'
+
+        caminho_completo_antigo = os.path.join(diretorio, nome_antigo)
+        caminho_completo_novo = os.path.join(diretorio, novo_nome)
+
+        os.rename(caminho_completo_antigo, caminho_completo_novo)
 # 4 - Repetir o passo 3 ate baixar todos os pdf
+baixar_relatorios_das_empresas(driver=driver)
+botao_proxima_pagina = driver.find_element(By.XPATH, "//button[@id='nextBtn']")
 
-
-
+while botao_proxima_pagina.get_attribute('disabled') == None:
+    botao_proxima_pagina.click()
+    baixar_relatorios_das_empresas(driver=driver)
 
 
